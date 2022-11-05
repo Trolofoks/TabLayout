e069d111292dc4f0c878c949af5eaea95366bf2c
Trolofoks <trolofoks@gmail.com>
2022-11-05 18:31:00 +0700

    day 1

--- app/src/main/java/com/example/tablayout/MainActivity.kt
+++ app/src/main/java/com/example/tablayout/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.tablayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.tablayout.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    //список с фрагментами
+    private val fragmentList = listOf(
+        FragmentNumber1.newInstance(),
+        FragmentNumber2.newInstance(),
+        FragmentNumber3.newInstance()
+    )
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //основная строчка для tabLayout
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            //три функции снизу имплементируются от обджекта
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(binding.placeHolder.id, fragmentList[tab?.position!!])
+                    .commit()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                //эти временно можно игнорить
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                //эти временно можно игнорить
+            }
+
+        })
     }
 }
